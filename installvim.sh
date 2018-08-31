97451bec7795c1bf2226f6df06ac0fbd7969ac5e
malr00t <malr00t@yandex.com>
2018-08-31 14:21:32 +0800

    modified installvim.sh from python3.5 to python3.6

--- installvim.sh
+++ installvim.sh
@@ -18,7 +18,7 @@ sudo apt-get install \
 		--enable-gui=gtk3 \
 		--enable-cscope \
 		--enable-python3interp=yes \
-		--with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu/ \
+		--with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/ \
 		--enable-pythoninterp=yes \
 		--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
 		--enable-perlinterp=yes \
